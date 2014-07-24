5355f65c9008622732e36910a3811f00564487fc
xiabai <xiabai@redhat.com>
2014-07-24 17:49:12 +0800

    REV: add $Inject KieWorkbenchACL for permission of project operation

--- drools-wb-webapp/src/main/java/org/drools/workbench/client/DroolsWorkbenchEntryPoint.java
+++ drools-wb-webapp/src/main/java/org/drools/workbench/client/DroolsWorkbenchEntryPoint.java
@@ -52,7 +52,9 @@ import org.uberfire.workbench.model.menu.MenuFactory;
 import org.uberfire.workbench.model.menu.MenuItem;
 import org.uberfire.workbench.model.menu.MenuPosition;
 import org.uberfire.workbench.model.menu.Menus;
-
+import org.kie.workbench.common.services.security.KieWorkbenchACL;
+import org.kie.workbench.common.services.shared.security.KieWorkbenchSecurityService;
+import org.kie.workbench.common.services.security.KieWorkbenchPolicy;
 import static org.uberfire.workbench.model.menu.MenuFactory.*;
 
 /**
@@ -61,159 +63,183 @@ import static org.uberfire.workbench.model.menu.MenuFactory.*;
 @EntryPoint
 public class DroolsWorkbenchEntryPoint {
 
-    @Inject
-    private Caller<AppConfigService> appConfigService;
+	@Inject
+	private Caller<AppConfigService> appConfigService;
 
-    @Inject
-    private WorkbenchMenuBarPresenter menubar;
+	@Inject
+	private WorkbenchMenuBarPresenter menubar;
 
-    @Inject
-    private PlaceManager placeManager;
+	@Inject
+	private PlaceManager placeManager;
 
-    @Inject
-    private SyncBeanManager iocManager;
+	@Inject
+	private SyncBeanManager iocManager;
 
-    @Inject
-    private ActivityManager activityManager;
+	@Inject
+	private ActivityManager activityManager;
 
-    @AfterInitialization
-    public void startApp() {
-        loadPreferences();
-        setupMenu();
-        hideLoadingPopup();
-    }
+	@Inject
+	private KieWorkbenchACL kieACL;
 
-    private void loadPreferences() {
-        UberFirePreferences.setProperty( "org.uberfire.client.workbench.widgets.listbar.context.disable", true );
-        appConfigService.call( new RemoteCallback<Map<String, String>>() {
-            @Override
-            public void callback( final Map<String, String> response ) {
-                ApplicationPreferences.setUp( response );
-            }
-        } ).loadPreferences();
-    }
+	@Inject
+	private Caller<KieWorkbenchSecurityService> kieSecurityService;
 
-    private void setupMenu() {
-        final AbstractWorkbenchPerspectiveActivity defaultPerspective = getDefaultPerspectiveActivity();
+	@AfterInitialization
+	public void startApp() {
+		kieSecurityService.call(new RemoteCallback<String>() {
+			public void callback(final String str) {
+				KieWorkbenchPolicy policy = new KieWorkbenchPolicy(str);
+				kieACL.activatePolicy(policy);
+				loadPreferences();
+				setupMenu();
+				hideLoadingPopup();
+			}
+		}).loadPolicy();
+	}
 
-        final Menus menus = MenuFactory
-                .newTopLevelMenu( AppConstants.INSTANCE.Home() )
-                .respondsWith( new Command() {
-                    @Override
-                    public void execute() {
-                        if ( defaultPerspective != null ) {
-                            placeManager.goTo( new DefaultPlaceRequest( defaultPerspective.getIdentifier() ) );
-                        } else {
-                            Window.alert( "Default perspective not found." );
-                        }
-                    }
-                } )
-                .endMenu()
-                .newTopLevelMenu( AppConstants.INSTANCE.Perspectives() )
-                .withItems( getPerspectives() )
-                .endMenu()
-                .newTopLevelMenu( AppConstants.INSTANCE.Logout())
-                .respondsWith( new Command() {
-                    @Override
-                    public void execute() {
-                        redirect( GWT.getModuleBaseURL() + "uf_logout" );
-                    }
-                } )
-                .endMenu()
-                .newTopLevelMenu( AppConstants.INSTANCE.Find() )
-                .position( MenuPosition.RIGHT )
-                .respondsWith( new Command() {
-                    @Override
-                    public void execute() {
-                        placeManager.goTo( "FindForm" );
-                    }
-                } )
-                .endMenu()
-                .newTopLevelCustomMenu( iocManager.lookupBean( CustomSplashHelp.class ).getInstance() )
-                .endMenu()
-                .build();
+	private void loadPreferences() {
+		UberFirePreferences
+				.setProperty(
+						"org.uberfire.client.workbench.widgets.listbar.context.disable",
+						true);
+		appConfigService.call(new RemoteCallback<Map<String, String>>() {
+			@Override
+			public void callback(final Map<String, String> response) {
+				ApplicationPreferences.setUp(response);
+			}
+		}).loadPreferences();
+	}
 
-        menubar.addMenus( menus );
-    }
+	private void setupMenu() {
+		final AbstractWorkbenchPerspectiveActivity defaultPerspective = getDefaultPerspectiveActivity();
 
-    private List<MenuItem> getPerspectives() {
-        final List<MenuItem> perspectives = new ArrayList<MenuItem>();
-        for ( final AbstractWorkbenchPerspectiveActivity perspective : getPerspectiveActivities() ) {
-            final String name = perspective.getPerspective().getName();
-            final Command cmd = new Command() {
+		final Menus menus = MenuFactory
+				.newTopLevelMenu(AppConstants.INSTANCE.Home())
+				.respondsWith(new Command() {
+					@Override
+					public void execute() {
+						if (defaultPerspective != null) {
+							placeManager.goTo(new DefaultPlaceRequest(
+									defaultPerspective.getIdentifier()));
+						} else {
+							Window.alert("Default perspective not found.");
+						}
+					}
+				})
+				.endMenu()
+				.newTopLevelMenu(AppConstants.INSTANCE.Perspectives())
+				.withItems(getPerspectives())
+				.endMenu()
+				.newTopLevelMenu(AppConstants.INSTANCE.Logout())
+				.respondsWith(new Command() {
+					@Override
+					public void execute() {
+						redirect(GWT.getModuleBaseURL() + "uf_logout");
+					}
+				})
+				.endMenu()
+				.newTopLevelMenu(AppConstants.INSTANCE.Find())
+				.position(MenuPosition.RIGHT)
+				.respondsWith(new Command() {
+					@Override
+					public void execute() {
+						placeManager.goTo("FindForm");
+					}
+				})
+				.endMenu()
+				.newTopLevelCustomMenu(
+						iocManager.lookupBean(CustomSplashHelp.class)
+								.getInstance()).endMenu().build();
 
-                @Override
-                public void execute() {
-                    placeManager.goTo( new DefaultPlaceRequest( perspective.getIdentifier() ) );
-                }
+		menubar.addMenus(menus);
+	}
 
-            };
-            final MenuItem item = newSimpleItem( name ).respondsWith( cmd ).endMenu().build().getItems().get( 0 );
-            perspectives.add( item );
-        }
+	private List<MenuItem> getPerspectives() {
+		final List<MenuItem> perspectives = new ArrayList<MenuItem>();
+		for (final AbstractWorkbenchPerspectiveActivity perspective : getPerspectiveActivities()) {
+			final String name = perspective.getPerspective().getName();
+			final Command cmd = new Command() {
 
-        return perspectives;
-    }
+				@Override
+				public void execute() {
+					placeManager.goTo(new DefaultPlaceRequest(perspective
+							.getIdentifier()));
+				}
 
-    private AbstractWorkbenchPerspectiveActivity getDefaultPerspectiveActivity() {
-        AbstractWorkbenchPerspectiveActivity defaultPerspective = null;
-        final Collection<IOCBeanDef<AbstractWorkbenchPerspectiveActivity>> perspectives = iocManager.lookupBeans( AbstractWorkbenchPerspectiveActivity.class );
-        final Iterator<IOCBeanDef<AbstractWorkbenchPerspectiveActivity>> perspectivesIterator = perspectives.iterator();
-        outer_loop:
-        while ( perspectivesIterator.hasNext() ) {
-            final IOCBeanDef<AbstractWorkbenchPerspectiveActivity> perspective = perspectivesIterator.next();
-            final AbstractWorkbenchPerspectiveActivity instance = perspective.getInstance();
-            if ( instance.isDefault() ) {
-                defaultPerspective = instance;
-                break outer_loop;
-            } else {
-                iocManager.destroyBean( instance );
-            }
-        }
-        return defaultPerspective;
-    }
+			};
+			final MenuItem item = newSimpleItem(name).respondsWith(cmd)
+					.endMenu().build().getItems().get(0);
+			perspectives.add(item);
+		}
 
-    private List<AbstractWorkbenchPerspectiveActivity> getPerspectiveActivities() {
+		return perspectives;
+	}
 
-        //Get Perspective Providers
-        final Set<AbstractWorkbenchPerspectiveActivity> activities = activityManager.getActivities( AbstractWorkbenchPerspectiveActivity.class );
+	private AbstractWorkbenchPerspectiveActivity getDefaultPerspectiveActivity() {
+		AbstractWorkbenchPerspectiveActivity defaultPerspective = null;
+		final Collection<IOCBeanDef<AbstractWorkbenchPerspectiveActivity>> perspectives = iocManager
+				.lookupBeans(AbstractWorkbenchPerspectiveActivity.class);
+		final Iterator<IOCBeanDef<AbstractWorkbenchPerspectiveActivity>> perspectivesIterator = perspectives
+				.iterator();
+		outer_loop: while (perspectivesIterator.hasNext()) {
+			final IOCBeanDef<AbstractWorkbenchPerspectiveActivity> perspective = perspectivesIterator
+					.next();
+			final AbstractWorkbenchPerspectiveActivity instance = perspective
+					.getInstance();
+			if (instance.isDefault()) {
+				defaultPerspective = instance;
+				break outer_loop;
+			} else {
+				iocManager.destroyBean(instance);
+			}
+		}
+		return defaultPerspective;
+	}
 
-        //Sort Perspective Providers so they're always in the same sequence!
-        List<AbstractWorkbenchPerspectiveActivity> sortedActivities = new ArrayList<AbstractWorkbenchPerspectiveActivity>( activities );
-        Collections.sort( sortedActivities,
-                          new Comparator<AbstractWorkbenchPerspectiveActivity>() {
+	private List<AbstractWorkbenchPerspectiveActivity> getPerspectiveActivities() {
 
-                              @Override
-                              public int compare( AbstractWorkbenchPerspectiveActivity o1,
-                                                  AbstractWorkbenchPerspectiveActivity o2 ) {
-                                  return o1.getPerspective().getName().compareTo( o2.getPerspective().getName() );
-                              }
+		// Get Perspective Providers
+		final Set<AbstractWorkbenchPerspectiveActivity> activities = activityManager
+				.getActivities(AbstractWorkbenchPerspectiveActivity.class);
 
-                          } );
+		// Sort Perspective Providers so they're always in the same sequence!
+		List<AbstractWorkbenchPerspectiveActivity> sortedActivities = new ArrayList<AbstractWorkbenchPerspectiveActivity>(
+				activities);
+		Collections.sort(sortedActivities,
+				new Comparator<AbstractWorkbenchPerspectiveActivity>() {
 
-        return sortedActivities;
-    }
+					@Override
+					public int compare(AbstractWorkbenchPerspectiveActivity o1,
+							AbstractWorkbenchPerspectiveActivity o2) {
+						return o1.getPerspective().getName()
+								.compareTo(o2.getPerspective().getName());
+					}
 
-    //Fade out the "Loading application" pop-up
-    private void hideLoadingPopup() {
-        final Element e = RootPanel.get( "loading" ).getElement();
+				});
 
-        new Animation() {
+		return sortedActivities;
+	}
 
-            @Override
-            protected void onUpdate( double progress ) {
-                e.getStyle().setOpacity( 1.0 - progress );
-            }
+	// Fade out the "Loading application" pop-up
+	private void hideLoadingPopup() {
+		final Element e = RootPanel.get("loading").getElement();
 
-            @Override
-            protected void onComplete() {
-                e.getStyle().setVisibility( Style.Visibility.HIDDEN );
-            }
-        }.run( 500 );
-    }
+		new Animation() {
 
-    public static native void redirect( String url )/*-{
-        $wnd.location = url;
-    }-*/;
+			@Override
+			protected void onUpdate(double progress) {
+				e.getStyle().setOpacity(1.0 - progress);
+			}
+
+			@Override
+			protected void onComplete() {
+				e.getStyle().setVisibility(Style.Visibility.HIDDEN);
+			}
+		}.run(500);
+	}
+
+	public static native void redirect(String url)/*-{
+													$wnd.location = url;
+													}-*/;
 
 }
